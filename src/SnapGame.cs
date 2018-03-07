566cb73c4921afe062ead4308c1f755fcf5ff3d7
keithbryanling <keithbryanling@outlook.my>
2018-03-07 12:15:43 +0800

    4th Team Leader Commit

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -17,14 +17,25 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
+		private static void HandleUserInput (Snap myGame)
 		{
 			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+			SwinGame.ProcessEvents ();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
+			if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
 				myGame.Start ();
+				myGame.FlipNextCard ();
+			}
+
+			if (myGame.IsStarted) {
+				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
+				   SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+
+				} else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
+					myGame.PlayerHit (0);
+				} else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					myGame.PlayerHit (1);
+				}
 			}
 		}
 
